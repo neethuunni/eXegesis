5a7655aae91dfea71e96a4b99a29aeb717c718e3
gokz <gokulnathgm@gmail.com>
2016-12-04 22:13:07 +0530

    commit 3

---
+++ public/controller.js
@@ -0,0 +1,11 @@
+angular.module('myApp', []).
+	controller('myAppCtrl', ['$scope', '$http',  function($scope, $http) {
+		console.log('controller ready');
+
+		$scope.send = function(){ 
+			console.log('clicked');
+			$http.get('/xml').success(function(response) {
+				console.log(response);
+			});
+		}
+	}]);
