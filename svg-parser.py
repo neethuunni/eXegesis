adbb4ff60b28a16f821a972957ce3995ecb21d35
Gokul Nath <gokulnath@qburst.com>
2016-12-16 16:56:41 +0530

    parses path data

--- svg-parser.py
+++ svg-parser.py
@@ -1,13 +1,15 @@
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('rect.svg')
+tree = ET.parse('sign-in2.svg')
 root = tree.getroot()
 translate = []
 annotations = []
 g_attributes = {}
 defs = {}
+trans_child = []
 
 def getSubChild(child):
+	global trans_child
 	for subchild in child:
 		elm = {}
 		attribute = subchild.attrib
@@ -15,6 +17,7 @@ def getSubChild(child):
 		if tag == 'g' and 'transform' in attribute.keys():
 			transform = attribute['transform']
 			if transform.startswith('translate'):
+				trans_child.append(subchild)
 				length = len(transform) - 1
 				transform = transform[10:length]
 				transform = transform.split(',')
@@ -38,7 +41,7 @@ def getSubChild(child):
 			if tag == 'tspan':
 				g_attributes.clear()
 				attribute['text'] = subchild.text
-			# print tag, attribute, '\n'
+			# print tag, attribute, translate, '\n'
 			attribute['type'] = tag
 			if g_attributes:
 				attribute.update(g_attributes)
@@ -76,10 +79,60 @@ def getSubChild(child):
 				defs_list[id]['y'] = float(defs_list[id]['y']) + translation[1]
 			annotations.append(defs_list[id])
 
+		if tag == 'path':
+			path_data = {}
+			path = attribute['d'].split(' ')
+			paths = []
+			for i in path:
+				if i == 'z' or i == 'Z':
+					continue
+				point = i.split(',')
+				if not point[0].replace('.', '').isdigit():
+					point[0] = point[0][1:]
+				point[0] = float(point[0])
+				point[1] = float(point[1])
+				paths.extend(point)
+			x_array = []
+			y_array = []
+			for j in range(len(paths)):
+				if j % 2 == 0:
+					x_array.append(paths[j])
+				else:
+					y_array.append(paths[j])
+			x_max = max(x_array)
+			y_max = max(y_array)
+			x_min = min(x_array)
+			y_min = min(y_array)
+			x = x_min
+			y = y_min
+			if translate:
+				if len(translate) > 2:
+					translation = [0] * 2
+					for i in range(len(translate)):
+						if i % 2 == 0:
+							translation[0] += translate[i]
+						else:
+							translation[1] += translate[i]
+				else:
+					translation = translate[:]
+				y = y_min + translation[1]
+				x = x_min + translation[0]
+			height = y_max - y_min
+			width = x_max - x_min
+			del attribute['d']
+			path_data.update(attribute)
+			path_data.update(g_attributes)
+			path_data['height'] = height
+			path_data['width'] = width
+			path_data['x'] = x
+			path_data['y'] = y
+			annotations.append(path_data)
+
 		getSubChild(subchild)
-		if translate:
+		if translate and subchild == trans_child[len(trans_child) - 1]:
 			translate.pop()
 			translate.pop()
+			trans_child.pop()
 	return annotations
 
 def getDefs(root):
@@ -93,10 +146,6 @@ def getDefs(root):
 				attribute['type'] = 'rect'
 				defs[id] = attribute
 
-# def getOuterChild(root):
-	# for child in root:
-
-
 def getChild(root):
 	for child in root:
 		tag = child.tag.split('}')[1]
@@ -106,9 +155,7 @@ def getChild(root):
 		elif len(child) > 0 and tag != 'title' and tag != 'desc' and tag != 'defs':
 			getSubChild(child)
 
-		# elif tag != 'title' and tag != 'desc' and tag != 'defs':
-		# 	getOuterChild(child)
 
 getChild(root)
-for i in annotations:
-	print i
+# for i in annotations:
+# 	print i
--- svg_parser/parse_svg/views.py
+++ svg_parser/parse_svg/views.py
@@ -11,16 +11,18 @@ translate = []
 annotations = []
 g_attributes = {}
 defs = {}
+trans_child = []
 
 def getSubChild(child):
+	global trans_child
 	for subchild in child:
 		elm = {}
 		attribute = subchild.attrib
 		tag = subchild.tag.split('}')[1]
-
 		if tag == 'g' and 'transform' in attribute.keys():
 			transform = attribute['transform']
 			if transform.startswith('translate'):
+				trans_child.append(subchild)
 				length = len(transform) - 1
 				transform = transform[10:length]
 				transform = transform.split(',')
@@ -44,7 +46,7 @@ def getSubChild(child):
 			if tag == 'tspan':
 				g_attributes.clear()
 				attribute['text'] = subchild.text
-			# print tag, attribute, translate
+			# print tag, attribute, translate, '\n'
 			attribute['type'] = tag
 			if g_attributes:
 				attribute.update(g_attributes)
@@ -82,10 +84,60 @@ def getSubChild(child):
 				defs_list[id]['y'] = float(defs_list[id]['y']) + translation[1]
 			annotations.append(defs_list[id])
 
+		if tag == 'path':
+			path_data = {}
+			path = attribute['d'].split(' ')
+			paths = []
+			for i in path:
+				if i == 'z' or i == 'Z':
+					continue
+				point = i.split(',')
+				if not point[0].replace('.', '').isdigit():
+					point[0] = point[0][1:]
+				point[0] = float(point[0])
+				point[1] = float(point[1])
+				paths.extend(point)
+			x_array = []
+			y_array = []
+			for j in range(len(paths)):
+				if j % 2 == 0:
+					x_array.append(paths[j])
+				else:
+					y_array.append(paths[j])
+			x_max = max(x_array)
+			y_max = max(y_array)
+			x_min = min(x_array)
+			y_min = min(y_array)
+			x = x_min
+			y = y_min
+			if translate:
+				if len(translate) > 2:
+					translation = [0] * 2
+					for i in range(len(translate)):
+						if i % 2 == 0:
+							translation[0] += translate[i]
+						else:
+							translation[1] += translate[i]
+				else:
+					translation = translate[:]
+				y = y_min + translation[1]
+				x = x_min + translation[0]
+			height = y_max - y_min
+			width = x_max - x_min
+			del attribute['d']
+			path_data.update(attribute)
+			path_data.update(g_attributes)
+			path_data['height'] = height
+			path_data['width'] = width
+			path_data['x'] = x
+			path_data['y'] = y
+			annotations.append(path_data)
+
 		getSubChild(subchild)
-		if translate:
+		if translate and subchild == trans_child[len(trans_child) - 1]:
 			translate.pop()
 			translate.pop()
+			trans_child.pop()
 	return annotations
 
 def getDefs(root):
